02ea0b68340f300c98cfeec7321ba96e63d38b2f
Rob Wong <robert.m.wong@espn.com>
2017-10-10 01:17:43 -0400

    updating dependencies don't include css in dev mode

--- src/server/views/shell.js
+++ src/server/views/shell.js
@@ -1,21 +1,24 @@
-export default (app = '', initialState = {}) => `<!doctype html>
-<html lang="en">
-  <head>
-    <title>Basically Fire</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-    <link href="/assets/app.bundle.css" rel="stylesheet" />
-    <style>
-      body {
-        font-family: 'Roboto', sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="root">${app}</div>
-    <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};</script>
-    <script src="/assets/bundle.js"></script>
-  </body>
-</html>
-`;
+export default (app = '', initialState = {}) => {
+  const css = (process.env.NODE_ENV === 'development') ? '': '<link href="/assets/app.bundle.css" rel="stylesheet" />';
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <title>Basically Fire</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+        ${css}
+        <style>
+          body {
+            font-family: 'Roboto', sans-serif;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="root">${app}</div>
+        <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};</script>
+        <script src="/assets/bundle.js"></script>
+      </body>
+    </html>
+  `;
+}
